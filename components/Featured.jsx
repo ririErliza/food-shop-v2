c9dff51d43a0b8ca5e5a0ce7d387ebfbf4d60517
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-11 11:50:02 +0100

    update

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const Featured = () => {
   const [index, setIndex] = useState(0);
   const images = [
-    "/img/featured.png",
+    "/img/featured1.png",
     "/img/featured2.png",
     "/img/featured3.png",
   ];
